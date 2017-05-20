fe2d6670073ba66ef1782cc5a161a6fd08c1f3f6
Nipuna <Nipuna@DESKTOP-3B1B6HJ>
2017-05-21 01:33:09 +0530

    create LoginClickEvent

--- UserManagement.UI/JS/LoginJS.js
+++ UserManagement.UI/JS/LoginJS.js
@@ -1,15 +1,20 @@
 ﻿var content = "";
+var activeComponentList;
 document.addEventListener("DOMContentLoaded", function (event) {
-    GetPageContentsList();
-    LoadElements();
+    GetPageContentsList('Login');
 
 });
 
-function GetPageContentsList() {
-    var request = new XMLHttpRequest();
-    request.open("get", "http://localhost:54213/api/custom/get", false);
-    request.send();
-    //var mydata = JSON.parse(request.);
+function GetPageContentsList(pageName) {
+    try {
+        var request = new XMLHttpRequest();
+        request.open("get", "http://localhost:54213/api/ScreenComponent/" + pageName, false);
+        request.send();
+        LoadElements();
+    } catch (e) {
+        throw "404";
+
+    }
 }
 
 
@@ -68,13 +73,44 @@ function LoadElements() {
     var passwordtextfield = '<input id="passwordtextfield" type="password" name="psw"><br>';
     var loginbutton = '<input type="submit" value="Login" onclick="LoginClickEvent()">';
 
-    var elementOrder = [usernamelabel, userNameTxtBox, passwordlabel, passwordtextfield, loginbutton];
-
+    //var elementOrder = [usernamelabel, userNameTxtBox, passwordlabel, passwordtextfield, loginbutton];
+    var elementOrder = ['usernamelabel', 'userNameTxtBox', 'passwordlabel', 'passwordtextfield', 'loginbutton'];
     for (var i = 0; i < elementOrder.length; i++) {
-        content = content.concat(elementOrder[i]);
+
+        if (activeComponentList.includes(elementOrder[i])) {
+            console.log("true");
+
+            switch (elementOrder[i]) {
+                case 'usernamelabel':
+                    content = content.concat(usernamelabel);
+                    break;
+                case 'userNameTxtBox':
+                    content = content.concat(userNameTxtBox);
+                    break;
+                case 'passwordlabel':
+                    content = content.concat(passwordlabel);
+                    break;
+                case 'passwordtextfield':
+                    content = content.concat(passwordtextfield);
+                    break;
+                case 'loginbutton':
+                    content = content.concat(loginbutton);
+                    break;
+                default:
+                    break;
+
+
+            }
+
+
+        } else {
+            console.log("false");
+
+        }
+
     }
 
-    
+
     var body = document.getElementById('body');
     body.innerHTML = content;
 }
@@ -83,4 +119,17 @@ function LoginClickEvent() {
     var userName = document.getElementById('userNameTxtBox').value;
     var password = document.getElementById('passwordtextfield').value;
     alert(userName);
+    var request = new XMLHttpRequest();
+    var params = { userName, password };
+    request.open("POST", "http://localhost:54213/api/ScreenComponent/", true);
+
+    //Send the proper header information along with the request
+    request.setRequestHeader('Content-Type', 'application/json');
+
+    request.onreadystatechange = function () {//Call a function when the state changes.
+        if (request.readyState == 4 && request.status == 200) {
+            alert(request.responseText);
+        }
+    }
+    request.send(JSON.stringify(params));
 }
